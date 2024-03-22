0b7a9382f3717d556326f317aef857575d9e7b95
Yu-huan Kuo <touch@rnmeow.com>
2024-03-22 20:47:58 +0800

    fix!: type errors when calling `app.use()`

--- src/middlewares/trailing-slash.ts
+++ src/middlewares/trailing-slash.ts
@@ -1,10 +1,12 @@
 import { createMiddleware } from 'hono/factory'
 
+import type { Env, MiddlewareHandler } from 'hono/types'
+
 /**
  * Trim the trailing slash from the URL if it does have one. This is only used when the route is set without a trailing slash.
  * @example hono.use(trimTrailingSlash())
  */
-export function trimTrailingSlash() {
+export function trimTrailingSlash(): MiddlewareHandler<Env, never, {}> {
 	return createMiddleware(async (ctx, next) =>
 		ctx.req.path.endsWith('/') && ctx.req.path !== '/'
 			? ctx.redirect(ctx.req.url.replace(/\/$/, ''))
@@ -16,7 +18,7 @@ export function trimTrailingSlash() {
  * Append a trailing slash to the URL if it does not have one. This is only used when the route is set with a trailing slash.
  * @example hono.use(appendTrailingSlash())
  */
-export function appendTrailingSlash() {
+export function appendTrailingSlash(): MiddlewareHandler<Env, never, {}> {
 	return createMiddleware(async (ctx, next) =>
 		!ctx.req.url.endsWith('/') ? ctx.redirect(`${ctx.req.url}/`) : await next(),
 	)
